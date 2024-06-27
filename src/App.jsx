e53d166b7ca9d4c3e4368e4c957b95bc737bd336
christo.sivi <christo.sivi@qburst.com>
2024-06-27 10:30:24 +0530

    uplaod and generate level 1 : Prints zip folder file paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import { Box } from "@mui/material";
-import UploadPage from "./modules/Upload/UploadPage";
+import React, { useState } from "react";
+import JSZip from "jszip";
+import "./App.css";
 
 function App() {
+  const [zipFile, setZipFile] = useState(null);
+
+  const handleFileUpload = (event) => {
+    setZipFile(event.target.files[0]);
+  };
+
+  const handleGenerate = async () => {
+    if (zipFile) {
+      const zip = new JSZip();
+      const content = await zip.loadAsync(zipFile);
+
+      content.forEach((relativePath, file) => {
+        console.log(relativePath, file);
+      });
+    } else {
+      console.log("No file uploaded.");
+    }
+  };
+
   return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "80%",
-      }}
-    >
-      <UploadPage />
-    </Box>
+    <div className="App">
+      <h1>Upload and Generate File List</h1>
+      <input type="file" accept=".zip" onChange={handleFileUpload} />
+      <button onClick={handleGenerate}>Generate</button>
+    </div>
   );
 }
 
