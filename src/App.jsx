b37113942a506c36cdf9ee45c086497837c88fe5
akshay.kumaramthodi <akshay.kumaramthodi@qburst.com>
2024-06-28 17:54:15 +0530

    csd

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,7 @@ import { Box } from "@mui/material";
 import UploadPage from "./modules/Upload/UploadPage";
 
 function App() {
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "80%",
-      }}
-    >
-      <UploadPage />
-    </Box>
-  );
+  return <UploadPage />;
 }
 
 export default App;
--- src/modules/Upload/UploadPage.jsx
+++ src/modules/Upload/UploadPage.jsx
@@ -89,131 +89,140 @@ function UploadPage() {
   };
 
   return (
-    <Container sx={{ mt: 4, borderRadius: "20px", width: "50%" }}>
-      <Paper sx={{ p: 5, pt: 4, borderRadius: "20px" }}>
-        <Typography variant="h4" gutterBottom>
-          Upload and Generate File List
-        </Typography>
-        <Box
-          sx={{
-            border: "3px dashed grey",
-            borderRadius: "10px",
-            p: 2,
-            textAlign: "center",
-            alignContent: "center",
-            mb: 2,
-            height: "200px",
-            backgroundColor: isDragging ? "red" : "transparent",
-            transition: "background-color 0.3s",
-          }}
-          onDrop={handleDrop}
-          onDragOver={handleDragOver}
-          onDragEnter={handleDragEnter}
-          onDragLeave={handleDragLeave}
-        >
-          <Typography variant="body1" sx={{ mb: 1 }}>
-            Upload and attach files to this project.
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "80%",
+      }}
+    >
+      <Container sx={{ mt: 4, borderRadius: "20px", width: "50%" }}>
+        <Paper sx={{ p: 5, pt: 4, borderRadius: "20px" }}>
+          <Typography variant="h4" gutterBottom>
+            Upload and Generate File List
           </Typography>
-          <input
-            accept=".zip"
-            style={{ display: "none" }}
-            id="file-upload"
-            type="file"
-            onChange={handleFileUpload}
-            ref={fileInputRef}
-          />
-          <label htmlFor="file-upload">
+          <Box
+            sx={{
+              border: "3px dashed grey",
+              borderRadius: "10px",
+              p: 2,
+              textAlign: "center",
+              alignContent: "center",
+              mb: 2,
+              height: "200px",
+              backgroundColor: isDragging ? "#f1f1f1" : "transparent",
+              transition: "background-color 0.3s",
+            }}
+            onDrop={handleDrop}
+            onDragOver={handleDragOver}
+            onDragEnter={handleDragEnter}
+            onDragLeave={handleDragLeave}
+          >
+            <Typography variant="body1" sx={{ mb: 1 }}>
+              Upload and attach files to this project.
+            </Typography>
+            <input
+              accept=".zip"
+              style={{ display: "none" }}
+              id="file-upload"
+              type="file"
+              onChange={handleFileUpload}
+              ref={fileInputRef}
+            />
+            <label htmlFor="file-upload">
+              <Button
+                variant="outlined"
+                component="span"
+                startIcon={<UploadFileIcon />}
+                sx={{
+                  borderRadius: "10px",
+                  height: "50px",
+                  fontSize: "18px",
+                  color: "black",
+                  border: "2px solid black",
+                  "&:hover": {
+                    border: "2px solid black",
+                    backgroundColor: "#f1f1f1",
+                  },
+                }}
+              >
+                Click here or Drop your files here
+              </Button>
+            </label>
+            <Typography variant="body1" sx={{ mt: 1 }}>
+              Maximum file size should be 50MB
+            </Typography>
+          </Box>
+          {zipFile && (
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                p: 1,
+                border: "2px solid black",
+                borderRadius: "10px",
+                mb: 2,
+              }}
+            >
+              <Chip label={zipFile.name} sx={{ mr: 1 }} />
+              <Typography variant="body2">
+                {zipFile.size / (1024 * 1024) < 1
+                  ? `${(zipFile.size / 1024).toFixed(2)} KB`
+                  : `${(zipFile.size / (1024 * 1024)).toFixed(2)} MB`}
+              </Typography>
+              <IconButton onClick={handleCancel} sx={{ ml: "auto" }}>
+                <HighlightOffIcon />
+              </IconButton>
+            </Box>
+          )}
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            gap={2}
+          >
+            <Button
+              variant="contained"
+              onClick={handleGenerate}
+              disabled={!zipFile}
+              sx={{
+                width: "100%",
+                height: "50px",
+                backgroundColor: "black",
+                borderRadius: "10px",
+              }}
+              startIcon={<AutoAwesomeIcon />}
+            >
+              Generate
+            </Button>
             <Button
               variant="outlined"
-              component="span"
-              startIcon={<UploadFileIcon />}
+              onClick={handleCancel}
               sx={{
-                borderRadius: "10px",
+                width: "100%",
                 height: "50px",
-                width: "300px",
-                fontSize: "18px",
                 color: "black",
-                border: "2px solid black",
+                borderRadius: "10px",
+                border: "1px solid grey",
                 "&:hover": {
-                  border: "2px solid black",
+                  border: "1px solid black",
                   backgroundColor: "#f1f1f1",
                 },
               }}
             >
-              {isDragging ? `Drop here` : `Click to upload`}
+              Cancel
             </Button>
-          </label>
-          <Typography variant="body1" sx={{ mt: 1 }}>
-            Maximum file size should be 50MB
-          </Typography>
-        </Box>
-        {zipFile && (
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              p: 1,
-              border: "2px solid black",
-              borderRadius: "10px",
-              mb: 2,
-            }}
-          >
-            <Chip label={zipFile.name} sx={{ mr: 1 }} />
-            <Typography variant="body2">
-              {zipFile.size / (1024 * 1024) < 1
-                ? `${(zipFile.size / 1024).toFixed(2)} KB`
-                : `${(zipFile.size / (1024 * 1024)).toFixed(2)} MB`}
-            </Typography>
-            <IconButton onClick={handleCancel} sx={{ ml: "auto" }}>
-              <HighlightOffIcon />
-            </IconButton>
           </Box>
-        )}
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          alignItems="center"
-          gap={2}
-        >
-          <Button
-            variant="contained"
-            onClick={handleGenerate}
-            disabled={!zipFile}
-            sx={{
-              width: "100%",
-              height: "50px",
-              backgroundColor: "black",
-              borderRadius: "10px",
-            }}
-            startIcon={<AutoAwesomeIcon />}
-          >
-            Generate
-          </Button>
-          <Button
-            variant="outlined"
-            onClick={handleCancel}
-            sx={{
-              width: "100%",
-              height: "50px",
-              color: "black",
-              borderRadius: "10px",
-              border: "1px solid grey",
-              "&:hover": {
-                border: "1px solid black",
-                backgroundColor: "#f1f1f1",
-              },
-            }}
-          >
-            Cancel
-          </Button>
-        </Box>
-        {error && (
-          <Typography variant="body1" color="error" sx={{ mt: 2 }}>
-            {error}
-          </Typography>
-        )}
-      </Paper>
-    </Container>
+          {error && (
+            <Typography variant="body1" color="error" sx={{ mt: 2 }}>
+              {error}
+            </Typography>
+          )}
+        </Paper>
+      </Container>
+    </Box>
   );
 }
 
