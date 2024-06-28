b7f703894bf548115498cb7fed2f342af7013951
akshay.kumaramthodi <akshay.kumaramthodi@qburst.com>
2024-06-28 19:37:57 +0530

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import UploadPage from "./modules/Upload/UploadPage";
+import ResultsPage from "./modules/Results/ResultsPage";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<UploadPage />} />
+      <Route path="results" element={<ResultsPage />} />
     </Routes>
   );
 }
